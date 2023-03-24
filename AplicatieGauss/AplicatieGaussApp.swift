2e8ed548fe1fb4180e809e3e3ac7c751d3f5d978
Cosmin Calaianu <cos_my9@yahoo.com>
2023-03-24 10:59:38 +0200

    Initial Commit

---
+++ AplicatieGauss/AplicatieGaussApp.swift
@@ -0,0 +1,17 @@
+//
+//  AplicatieGaussApp.swift
+//  AplicatieGauss
+//
+//  Created by Ariana Plitea on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AplicatieGaussApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
